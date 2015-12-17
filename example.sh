a1ff1f48ca54108fad163e421309359043bd9588
Godzil <godzil@godzil.net>
2015-12-17 11:32:02 +0000

    Create example.sh
    Add a simple example

---
+++ example.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+# Include the library
+. bashfblib.sh
+
+
+# Usage: ./example.sh min max r g b
+
+for y in `seq $1 $2`; do
+	for x in `seq $1 $2`; do
+		drawpixel $x $y $3 $4 $5
+	done
+done
+
